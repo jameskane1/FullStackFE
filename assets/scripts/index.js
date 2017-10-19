44e49c3aa606fd602eecd047f1588bb18647e1b5
jameskane1 <james.kane@libertymutual.com>
2017-10-19 15:25:28 -0400

    Added initial FE steps

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,27 @@ const config = require('./config')
 
 $(() => {
   setAPIOrigin(location, config)
+
+  // sign up button functionality
+  $('#sign-up-button').click(function () {
+    $('#signUpForm').removeClass('signup')
+    $('#sign-up-button').addClass('signup')
+  })
+  // sign in button functionality
+  $('#sign-in-button').click(function () {
+    $('#signInForm').removeClass('signin')
+    $('#sign-in-button').addClass('signin')
+  })
+
+  $('#hideSignUp').click(function () {
+    $('#signUpForm').addClass('signup')
+    $('#sign-up-button').removeClass('signup')
+  })
+
+  $('#hideSignIn').click(function () {
+    $('#signInForm').addClass('signin')
+    $('#sign-in-button').removeClass('signin')
+  })
 })
 
 // use require with a reference to bundle the file and use it in this file
