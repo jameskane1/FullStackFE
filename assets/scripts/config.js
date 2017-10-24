a058b868e87f1a10755d5976a06eaada4a59e9b4
jameskane1 <james.kane@libertymutual.com>
2017-10-24 09:40:12 -0400

    Revertered back to original prod config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'https://git.heroku.com/jimmy-kane-full-stack-project.git'
+    production: 'https://jimmy-kane-full-stack-project.herokuapp.com'
   }
 }
 
